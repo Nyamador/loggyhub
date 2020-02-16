08909ca9b780e171ec1d483025499a110b7248b7
Desmond <48738520+Nyamador@users.noreply.github.com>
2020-02-16 21:20:58 +0000

    Update settings.py

--- githubed/settings.py
+++ githubed/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False')
 
 ALLOWED_HOSTS = ['loggyhub.herokuapp.com/', '127.0.0.1']
 
